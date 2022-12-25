4098cf4513650d5ddd87f5ab78f50f09d42c0501
detbin <116663162+detbin@users.noreply.github.com>
2022-12-25 21:07:59 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-const winston = require('winston');
-
-const logger = winston.createLogger({
-  level: 'debug',
-  format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
-  transports: [
-    //
-    // - Write all logs with importance level of `error` or less to `error.log`
-    // - Write all logs with importance level of `info` or less to `combined.log`
-    //
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
-  ],
-});
-module.exports=logger;
+const logger = require('./logger')
+console.log('creciendo como desarrollador')
+try {
+    const div= (valor=>{return valor/2})
+    const a=numero
+    console.log(div(a))
+   } catch (e) { 
+        console.log(`Error del tipo: ${e}`)
+        }
